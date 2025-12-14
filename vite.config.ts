6e6905bb2be65fc510a89b1d0c2a56e3ed03c69f
Nate Dorleus <natebandz@NateBandzz-MBP.lan>
2025-12-14 14:40:29 -0500

    Revert to default GitHub Pages URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages
-  // Using "/" for custom domain (GaramiJiujitsu.com)
-  base: '/',
+  // Base path for GitHub Pages - matches repository name
+  base: '/GaramiJiujitsu/',
 })
 
